bbba564d48eb382e616caaa84dbb2eee36220e35
GUILHERME DINIZ CORREA <guilherme.dcorrea1@senacsp.edu.br>
2025-03-25 22:13:49 -0300

    banco de dados

---
+++ BancoDeDados/comandos.sql
@@ -0,0 +1,19 @@
+SELECT 
+    *
+FROM
+    usuario
+WHERE
+    id = 1;
+    
+    SELECT 
+    *
+FROM
+    usuario
+WHERE
+    email LIKE '%gmail.com';
+        SELECT 
+    *
+FROM
+    usuario
+WHERE
+    senha LIKE '%@%';
