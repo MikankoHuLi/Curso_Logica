789213816b937da4441d6935fc283716ca6cb745
GUILHERME DINIZ CORREA <guilherme.dcorrea1@senacsp.edu.br>
2025-04-14 19:30:54 -0300

    atualizar pasta

--- ListaAtividades/obj/Debug/net8.0-windows/ListaAtividades.AssemblyInfo.cs
+++ ListaAtividades/obj/Debug/net8.0-windows/ListaAtividades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42161032a64c49c9ad28d1378cd7eb53a58a24c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372205b94ad9c69d42b910f66fc1db7f24a65643")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListaAtividades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
